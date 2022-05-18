f2478731c161f2ee583361862628cbc17c10f5a2
Tyler Ross <tross020890@gmail.com>
2022-05-18 00:21:49 -0500

    Feat: can create new item

--- spec/requests/api/v1/items_requests_spec.rb
+++ spec/requests/api/v1/items_requests_spec.rb
@@ -24,6 +24,49 @@ describe "Items API" do
       expect(item[:description]).to be_a(String)
       expect(item).to have_key(:unit_price)
       expect(item[:unit_price]).to be_a(Float)
+      expect(item).to have_key(:merchant_id)
+      expect(item[:merchant_id]).to be_an(Integer)
     end
+  end
+  it 'sends Item by id' do
+    id = create(:merchant).id
+    item0 = create :item, {merchant_id: id}
+
+    get "/api/v1/items/#{item0.id}"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(Integer)
+      expect(item).to have_key(:name)
+      expect(item[:name]).to be_a(String)
+      expect(item).to have_key(:description)
+      expect(item[:description]).to be_a(String)
+      expect(item).to have_key(:unit_price)
+      expect(item[:unit_price]).to be_a(Float)
+      expect(item).to have_key(:merchant_id)
+      expect(item[:merchant_id]).to be_an(Integer)
+    end
+  it 'creates new Item' do
+    id = create(:merchant).id
+    item_params = ({
+                  name: 'Empty can of Pepsi MAX',
+                  description: 'Can of Pepsi Max, opened, contents gaseous ONLY',
+                  unit_price: 17,
+                  merchant_id: id,
+                })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+
   end
 end
