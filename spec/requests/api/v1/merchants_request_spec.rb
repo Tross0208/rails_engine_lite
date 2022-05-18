55b24e27bba6a595797041a833d8cdacf0e65d67
Tyler Ross <tross020890@gmail.com>
2022-05-17 22:50:31 -0500

    Sends merchant by id

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -16,7 +16,18 @@ describe "Merchant API" do
       expect(merchant).to have_key(:name)
       expect(merchant[:name]).to be_a(String)
     end
-
   end
+  it 'sends merchant by id' do
+    id = create(:merchant).id
 
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_an(Integer)
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
+  end
 end
