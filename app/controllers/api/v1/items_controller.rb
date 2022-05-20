c3fec5d17818cc857f2d544fa6920d923429a0b1
Tyler Ross <tross020890@gmail.com>
2022-05-20 08:45:34 -0500

    Feat: updates and destoys items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,14 +1,26 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    render json: ItemsSerializer.format_items(Item.all), status: :ok
+    render json: ItemSerializer.new(Item.all).serializable_hash.to_json
   end
 
   def show
-    render json: ItemsSerializer.format_items(Item.find(params[:id])), status: :ok
+    render json: ItemSerializer.new(Item.find(params[:id])).serializable_hash.to_json
   end
 
   def create
-    render json: ItemsSerializer.format_items(Item.create(item_params)), status: :ok
+    render json: ItemSerializer.new(Item.create(item_params)).serializable_hash.to_json, status: :created
+  end
+
+  def update
+    if Merchant.find(params[:merchant_id]) #|| !item_params[:merchant_id]
+      render json: ItemSerializer.new(Item.update(params[:id], item_params)).serializable_hash.to_json, status: 202
+    else
+      render status: 404
+    end
+  end
+
+  def destroy
+    render json: ItemSerializer.new(Item.delete(params[:id]))
   end
 
 private
--- spec/requests/api/v1/items_requests_spec.rb
+++ spec/requests/api/v1/items_requests_spec.rb
@@ -15,17 +15,17 @@ describe "Items API" do
 
     items = JSON.parse(response.body, symbolize_names: true)
 
-    items.each do |item|
+    items[:data].each do |item|
       expect(item).to have_key(:id)
-      expect(item[:id]).to be_an(Integer)
-      expect(item).to have_key(:name)
-      expect(item[:name]).to be_a(String)
-      expect(item).to have_key(:description)
-      expect(item[:description]).to be_a(String)
-      expect(item).to have_key(:unit_price)
-      expect(item[:unit_price]).to be_a(Float)
-      expect(item).to have_key(:merchant_id)
-      expect(item[:merchant_id]).to be_an(Integer)
+      expect(item[:id]).to be_an(String)
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_an(Integer)
     end
   end
   it 'sends Item by id' do
@@ -38,16 +38,16 @@ describe "Items API" do
 
     item = JSON.parse(response.body, symbolize_names: true)
 
-      expect(item).to have_key(:id)
-      expect(item[:id]).to be_an(Integer)
-      expect(item).to have_key(:name)
-      expect(item[:name]).to be_a(String)
-      expect(item).to have_key(:description)
-      expect(item[:description]).to be_a(String)
-      expect(item).to have_key(:unit_price)
-      expect(item[:unit_price]).to be_a(Float)
-      expect(item).to have_key(:merchant_id)
-      expect(item[:merchant_id]).to be_an(Integer)
+      expect(item[:data]).to have_key(:id)
+      expect(item[:data][:id]).to be_an(String)
+      expect(item[:data][:attributes]).to have_key(:name)
+      expect(item[:data][:attributes][:name]).to be_a(String)
+      expect(item[:data][:attributes]).to have_key(:description)
+      expect(item[:data][:attributes][:description]).to be_a(String)
+      expect(item[:data][:attributes]).to have_key(:unit_price)
+      expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+      expect(item[:data][:attributes]).to have_key(:merchant_id)
+      expect(item[:data][:attributes][:merchant_id]).to be_an(Integer)
     end
   it 'creates new Item' do
     id = create(:merchant).id
@@ -67,6 +67,35 @@ describe "Items API" do
     expect(created_item.description).to eq(item_params[:description])
     expect(created_item.unit_price).to eq(item_params[:unit_price])
     expect(created_item.merchant_id).to eq(item_params[:merchant_id])
-
   end
+
+  it 'updates Item' do
+    merch_id = create(:merchant).id
+    item0 = create :item, {merchant_id: merch_id}
+    id = item0.id
+    previous_name = Item.last.name
+    item_params = {name: 'New Name'}
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate(item: item_params)
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item0.name).to_not eq(previous_name)
+    expect(item0.name).to eq("New Name")
+  end
+
+  it 'destroys Item' do
+    merch_id = create(:merchant).id
+    item0 = create :item, {merchant_id: merch_id}
+
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{item0.id}"
+
+    expect(response).to be_successful
+    expect(Item.count).to eq(0)
+    expect{Item.find(item0.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
 end
